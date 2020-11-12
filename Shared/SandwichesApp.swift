0eb59e5e7f4534c9d34fb7abba7388de7f1d7343
Thy Sok <thysok@gmail.com>
2020-11-11 20:36:02 -0800

    Initial commit.

---
+++ Shared/SandwichesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandwichesApp.swift
+//  Shared
+//
+//  Created by Thy Sok on 11/11/20.
+//
+
+import SwiftUI
+
+@main
+struct SandwichesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
